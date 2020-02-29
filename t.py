d6e933ebaeb4886fc80ac44d6c4585c1f0cbac1b
best2000 <silentman@hotmail.co.th>
2020-03-01 03:34:14 +0700

    lol

--- t.py
+++ t.py
@@ -9,17 +9,33 @@ class telegram_bot:
 
     #All types used in the Bot API responses are represented as JSON-objects.
 
-    def getUpdates(self):
-        url = 'https://api.telegram.org/'+self.token+'/getUpdates'
+    def getUpdates(self, parameter):
+        url = 'https://api.telegram.org/'+self.token+'/getUpdates'+parameter
         res = requests.get(url)
         resdic = res.json() #convert json respond to python dict
+        print(resdic)     
+
+    #WEBHOOK
+    def setWebhook(self, parameter):
+        url = 'https://api.telegram.org/'+self.token+'/setWebhook'+parameter
+        res = requests.post(url)
+        resdic = res.json() 
         print(resdic)
-        #for i in resdic['result']:
-            #print(datetime.utcfromtimestamp(i['message']['date']).strftime('%Y-%m-%d %H:%M:%S'), ":", i['message']['text'])
+
+    def getWebhookInfo(self):
+        url = 'https://api.telegram.org/'+self.token+'/getWebhookInfo'
+        res = requests.get(url)
+        resdic = res.json() 
+        print(resdic)
+
+    def deleteWebhook(self):
+        url = 'https://api.telegram.org/'+self.token+'/deleteWebhook'
+        res = requests.get(url)
+        resdic = res.json() 
+        print(resdic)
 
 bot = telegram_bot('bot'+'1096258874:AAHtnfJpdm2_FPCHLChYHaEnZutzRsX7usU')
-bot.Update()
+upid = 383926255
+while True:
+    bot.getUpdates("?offset=383926255")
 
-#data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
-#headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-#r = requests.post(url, data=json.dumps(data), headers=headers)
